ff99bb886654ee5a545e3127422d27dd5123776f
VasuRaina <50510741+VasuRaina@users.noreply.github.com>
2022-09-10 17:03:53 -0700

    second commit

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -5,24 +5,42 @@ const LandingPage = () => {
   return (
     <Box display='flex' justifyContent='center' minHeight='90vh'>
       <Box
-        bgcolor='lawngreen'
         width={1080}
         alignItems='center'
         display='flex'
         padding={3}
         flexDirection='column'
       >
-        {/*  */}
-        {/* <Typography variant='h3' >Landing Page</Typography> */}
         <Box paddingX={3} width='50%'>
-          <Typography>
+          <Typography variant='subtitle1'>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
             aliquip ex ea commodo consequat.
           </Typography>
         </Box>
-        <Box>list</Box>
+        <Box paddingTop={4} width='50%'>
+          <Box marginY={3}>
+            <Typography variant='h4'>Vasu Raina</Typography>
+            <Typography variant='h5'>Team Lead</Typography>
+          </Box>
+          <Box marginY={3}>
+            <Typography variant='h4'>Kartikey Sarode</Typography>
+            <Typography variant='h5'>Github Master</Typography>
+          </Box>
+          <Box marginY={3}>
+            <Typography variant='h4'>Akash Thiagarajan</Typography>
+            <Typography variant='h5'>Back-End Lead</Typography>
+          </Box>
+          <Box marginY={3}>
+            <Typography variant='h4'>Kiran Shrestha</Typography>
+            <Typography variant='h5'>Front-End Lead</Typography>
+          </Box>
+          <Box marginY={3}>
+            <Typography variant='h4'>Sean Locklar</Typography>
+            <Typography variant='h5'>Scrum Master</Typography>
+          </Box>
+        </Box>
       </Box>
     </Box>
   );
