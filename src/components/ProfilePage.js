daaaaa802efd9214e5a2732319665e4d0f3d49f9
VasuRaina <50510741+VasuRaina@users.noreply.github.com>
2022-09-10 17:34:22 -0700

    profile page js

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Box, Typography } from '@mui/material';
 
 const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  //
+  const getUrlParam = () => {
+    const url = window.location.href;
+
+    let arr = url.split('/');
+
+    let profileName = arr[arr.length - 1];
+  };
+
+  useEffect(() => {
+    getUrlParam();
+  }, []);
+
+  return (
+    <Box display='flex' justifyContent='center' minHeight='90vh'>
+      <Box
+        width={1080}
+        alignItems='center'
+        display='flex'
+        padding={3}
+        flexDirection='column'
+      >
+        <Typography variant='subtitle1'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </Typography>
+      </Box>
+    </Box>
+  );
 };
 
 export default ProfilePage;
--- src/index.js
+++ src/index.js
@@ -17,8 +17,4 @@ const App = () => {
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
